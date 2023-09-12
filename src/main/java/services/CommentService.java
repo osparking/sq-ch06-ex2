f95be3dc3b19acfd70344a00a807514eacfef6b3
osparking <jbpark03@gmail.com>
2023-09-12 15:36:06 +0900

    comment use case : publishing

---
+++ src/main/java/services/CommentService.java
@@ -0,0 +1,20 @@
+package services;
+
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import model.Comment;
+import util.MyUtil;
+
+@Service
+public class CommentService {
+	Logger logger = Logger.getLogger(CommentService.class.getName());
+	
+	public void publishComment(Comment comment) {
+		logger.addHandler(MyUtil.handler);
+		logger.setUseParentHandlers(false);
+		logger.info("출판한 코맨트: " + comment.getText());
+	}
+
+}
