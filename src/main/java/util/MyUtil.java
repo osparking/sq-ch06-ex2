a703db6bdcdb2c6d494a7fb711780daf13f44000
osparking <jbpark03@gmail.com>
2023-09-14 14:43:17 +0900

    make util logger() do work as much as possible

--- src/main/java/util/MyUtil.java
+++ src/main/java/util/MyUtil.java
@@ -2,17 +2,21 @@ package util;
 
 import java.io.UnsupportedEncodingException;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
 
 public class MyUtil {
 	public static ConsoleHandler handler;
-	static {
+
+	public static Logger logger(String className) {
+		Logger logger = Logger.getLogger(className);
+		handler = new ConsoleHandler();
 		try {
-			handler = new ConsoleHandler();
 			handler.setEncoding("UTF-8");
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+		} catch (SecurityException | UnsupportedEncodingException e1) {
+			e1.printStackTrace();
 		}
+		logger.addHandler(handler);
+		logger.setUseParentHandlers(false);
+		return logger;
 	}
 }
